a8d485f0d7d55ef795be68e3b06e954f9c7cdbe2
alectramell <alectramell@gmail.com>
2017-01-14 06:09:59 -0800

    darkstar

--- main.sh
+++ main.sh
@@ -17,7 +17,7 @@ DSTRAILER() {
 	clear
 	notify-send --urgency="critical" --icon="/home/$USERNAME/Desktop/darkstar/darkstar.svg" "DarkStar" "..Press [ESC] to End Game Trailer.."
 	clear
-	wget https://github.com/alectramell/darkstar/raw/master/darkstar.mp4 -O - | mplayer -fs
+	mplayer $(youtube-dl -g https://github.com/alectramell/darkstar/raw/master/darkstar.mp4)
 	clear
 	sleep 0.5
 	clear
